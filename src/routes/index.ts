dd78f6cf6f264ce4b48745163ed02bf7d4134eee
Winterson <u19cse1028@cit.ac.in>
2025-07-18 23:54:18 +0530

    modify: use OpenAPIHono instance

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 /**
  * Custom Modules
  */
 import v1Routes from "./v1";
-
-const v1Router = new Hono().route("/v1", v1Routes);
-
+const v1Router = new OpenAPIHono().route("/v1", v1Routes);
 export default v1Router;
