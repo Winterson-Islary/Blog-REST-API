a0a45688ad8bcbc901fb5bef6f7725d5df5bba90
Winterson <u19cse1028@cit.ac.in>
2025-07-18 23:57:15 +0530

    modify: health.route.ts to create routes using openapi(createRoute()) method to make the routes document-able

--- src/routes/v1/health.route.ts
+++ src/routes/v1/health.route.ts
@@ -1,9 +1,49 @@
-import { Hono } from "hono";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import z from "zod";
 /**
  * Custom Modules
  */
 import { healthController, healthErrorController } from "@/controllers";
+import { createResponseDtoSchema } from "@/dtos";
 
-export const healthRoute = new Hono()
-	.get("/", healthController)
-	.get("/error", healthErrorController);
+// const healthRoute = new OpenAPIHono()
+// 	.get("/", healthController)
+// 	.get("/error", healthErrorController);
+
+const healthRoute = new OpenAPIHono()
+	.openapi(
+		createRoute({
+			method: "get",
+			path: "/",
+			responses: {
+				200: {
+					content: {
+						"application/json": {
+							schema: createResponseDtoSchema(z.string()),
+						},
+					},
+					description: "Returns a success message",
+				},
+			},
+		}),
+		healthController,
+	)
+	.openapi(
+		createRoute({
+			method: "get",
+			path: "/error",
+			responses: {
+				500: {
+					content: {
+						"application/json": {
+							schema: createResponseDtoSchema(z.null()),
+						},
+					},
+					description: "Simulates an error",
+				},
+			},
+		}),
+		healthErrorController,
+	);
+
+export { healthRoute };
