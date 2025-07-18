a98d1224b3f0545dd938a3cc24da7a748202f56b
Winterson <u19cse1028@cit.ac.in>
2025-07-18 23:56:00 +0530

    modify: modify createResponse to take  in status code and also set default codes in response

--- src/utils/response-formatter.ts
+++ src/utils/response-formatter.ts
@@ -1,22 +1,27 @@
 import type { Result } from "neverthrow";
 import type { ResponseDTO } from "@/dtos";
+import type { ContentfulStatusCode } from "hono/utils/http-status";
+import { StatusCodes } from "http-status-codes";
 /**
  * Custom Modules
  */
 import type { AppError } from "@/models/errors";
+import { env } from "@/config";
 
 export function createResponse<T>(
 	payload: Result<T, AppError>,
 	reqPath: string,
+	statusCode?: ContentfulStatusCode,
 	location?: string,
 	meta?: Record<string, unknown>,
 ): ResponseDTO<T | null> {
-	const isDev = process.env.NODE_ENV !== "production";
+	const isDev = env.NODE_ENV !== "production";
 
 	if (payload.isOk()) {
 		return {
 			success: true,
 			message: "Operation successful",
+			code: statusCode || StatusCodes.OK,
 			data: payload.value,
 			location,
 			meta,
@@ -30,6 +35,8 @@ export function createResponse<T>(
 	return {
 		success: false,
 		message: error.message,
+		code:
+			(error.code as ContentfulStatusCode) || StatusCodes.INTERNAL_SERVER_ERROR,
 		errorStack: isDev ? error.errorStack : undefined,
 		stackTrace:
 			isDev && error.stack
